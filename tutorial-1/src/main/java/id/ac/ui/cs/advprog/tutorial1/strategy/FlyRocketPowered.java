7435399086a7e1b428ad6fea7f9689dc900f589a
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-02-19 13:41:54 +0700

    Completed all classes that use FlyBehavior to implement flying behavior in duck

--- tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/FlyRocketPowered.java
+++ tutorial-1/src/main/java/id/ac/ui/cs/advprog/tutorial1/strategy/FlyRocketPowered.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.advprog.tutorial1.strategy;
 
-public class FlyRocketPowered {
+public class FlyRocketPowered implements FlyBehavior {
     // TODO Complete me!
+    public void fly() {
+        System.out.println("I'm flying with a rocket");
+    }
 }
