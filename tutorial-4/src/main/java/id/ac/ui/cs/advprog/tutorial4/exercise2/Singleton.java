11d6588696158ef1e5d698079fcd430633a0fe00
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-03-08 14:00:44 +0700

    Implemented Singleton class

--- tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise2/Singleton.java
+++ tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise2/Singleton.java
@@ -4,9 +4,12 @@ public class Singleton {
 
     // TODO Implement me!
     // What's missing in this Singleton declaration?
+    private static final Singleton INSTANCE = new Singleton();
+
+    private Singleton() {}
 
     public static Singleton getInstance() {
         // TODO Implement me!
-        return null;
+        return INSTANCE;
     }
 }
