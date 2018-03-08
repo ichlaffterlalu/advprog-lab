ab1f07f49ec4088561a9533497c6561af6db4658
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-03-08 13:33:07 +0700

    Created new concrete ingredient classes

---
+++ tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise1/factory/cheese/KraftCheese.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.tutorial4.exercise1.factory.cheese;
+
+public class KraftCheese implements Cheese {
+
+    public String toString() {
+        return "Melted Kraft Cheddar Cheese";
+    }
+}
