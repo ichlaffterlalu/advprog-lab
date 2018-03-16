a47d66bc8ff755293fd64593bcedee20e269a69b
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-03-16 20:33:18 +0700

    Implemented tests for generic app runner to make 100% coverage

---
+++ tutorial-5/src/test/java/hello/GenericRunnerTest.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class GenericRunnerTest {
+
+    @Test
+    public void applicationRunTest()
+    {
+        Application.main(new String[]{
+                "--spring.main.web-environment=false",
+                "--spring.autoconfigure.exclude=blahblahblah",
+                // Override any other environment properties according to your needs
+        });
+    }
+}
